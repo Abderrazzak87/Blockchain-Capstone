c356bca0f2580ba2c562ba10daea9530492fe345
Abderrazzak87 <abd.benabdallah@gmail.com>
2019-09-29 18:47:37 +0200

    EVO#1 : Add eth-contracts repository  Author : Abderrazzak BENABDALLAH

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var Verifier = artifacts.require("./Verifier.sol");
+var CapstoneERC721Token = artifacts.require("./CapstoneERC721Token.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+module.exports = async (deployer) => {
+  await deployer.deploy(Verifier);
+  await deployer.deploy(CapstoneERC721Token, "SS_ERC721MintableToken", "SS_721M");
+  await deployer.deploy(SolnSquareVerifier, Verifier.address, "SS_ERC721MintableToken", "SS_721M");
 };
